3173c463caaac471585c80e06ac4bb4c8cb380b1
GBDiso81 <Gbdiso81@gmail.com>
2025-12-18 04:39:20 +0800

    CarInsuramce

---
+++ CarInsurance/CarInsurance/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace CarInsurance.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection") { }
+
+        public DbSet<Insuree> Insurees { get; set; }
+    }
+}
